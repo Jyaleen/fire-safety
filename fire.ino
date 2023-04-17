a5e48e8be5e47b9568af903ecffc3895d0712ac8
Jyaleen <47906710+Jyaleen@users.noreply.github.com>
2023-04-17 18:15:18 -0400

    replacing red LED with neopixel

--- fire.ino
+++ fire.ino
@@ -1,4 +1,9 @@
+#include "FastLED.h"
 
+#define PIN_NEO_PIXEL  8   // Arduino pin that connects to NeoPixel
+#define NUM_LEDS  21
+
+CRGB leds[NUM_LEDS];
 int photoresistor = 0;              //this variable will hold a value based on the brightness of the ambient light
 int threshold = 900;                //if the photoresistor reading is below this value the the light will turn on
 int counter = 0;                   //how many times has the photoresister been hit?
@@ -6,8 +11,7 @@ int counter = 0;                   //how many times has the photoresister been h
 void setup()
 {
   Serial.begin(9600);               //start a serial connection with the computer
-
-  pinMode(13, OUTPUT);              //set pin 13 as an output that can be set to HIGH or LOW
+  FastLED.addLeds<NEOPIXEL, PIN_NEO_PIXEL>(leds, NUM_LEDS);
 }
 
 void loop()
@@ -15,13 +19,80 @@ void loop()
   //read the brightness of the ambient light
   photoresistor = analogRead(A0);   //set photoresistor to a number between 0 and 1023 based on how bright the ambient light is
   Serial.println(photoresistor);    //print the value of photoresistor in the serial monitor on the computer
+  Serial.println(counter);
 
-  //if the photoresistor value is higher the threshold turn the light on, otherwise turn it off
-  if ((photoresister > threshold) && (counter >= 3)) {
-    digitalWrite(13, HIGH);       // Turn on the LED
-    counter = 0;
-  } else {
-    digitalWrite(13, LOW);        // Turn off the LED
+  // if the number of times the fire extinguisher has been moved over the photoresistor exceeds 25, the fire goes out
+  if (counter >= 25) {
+    FastLED.clear(true);
+  } else if ((photoresistor > threshold) && (counter < 25)) {
+    counter++;
+    Fire(55,120,15);
+  }
+  else {
+    Fire(55,120,15);
   }
   delay(100);                       //short delay to make the printout easier to read
+}
+
+
+void Fire(int Cooling, int Sparking, int SpeedDelay) {
+  static byte heat[NUM_LEDS];
+  int cooldown;
+  
+  // Step 1.  Cool down every cell a little
+  for( int i = 0; i < NUM_LEDS; i++) {
+    cooldown = random(0, ((Cooling * 10) / NUM_LEDS) + 2);
+    
+    if(cooldown>heat[i]) {
+      heat[i]=0;
+    } else {
+      heat[i]=heat[i]-cooldown;
+    }
+  }
+  
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( int k= NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  }
+    
+  // Step 3.  Randomly ignite new 'sparks' near the bottom
+  if( random(255) < Sparking ) {
+    int y = random(7);
+    heat[y] = heat[y] + random(160,255);
+    //heat[y] = random(160,255);
+  }
+
+  // Step 4.  Convert heat to LED colors
+  for( int j = 0; j < NUM_LEDS; j++) {
+    setPixelHeatColor(j, heat[j] );
+  }
+
+  FastLED.show();
+  delay(SpeedDelay);
+}
+
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+  // FastLED
+  leds[Pixel].r = red;
+  leds[Pixel].g = green;
+  leds[Pixel].b = blue;
+}
+
+void setPixelHeatColor (int Pixel, byte temperature) {
+  // Scale 'heat' down from 0-255 to 0-191
+  byte t192 = round((temperature/255.0)*191);
+ 
+  // calculate ramp up from
+  byte heatramp = t192 & 0x3F; // 0..63
+  heatramp <<= 2; // scale up to 0..252
+ 
+  // figure out which third of the spectrum we're in:
+  if( t192 > 0x80) {                     // hottest
+    setPixel(Pixel, 255, 255, heatramp);
+  } else if( t192 > 0x40 ) {             // middle
+    setPixel(Pixel, 255, heatramp, 0);
+  } else {                               // coolest
+    setPixel(Pixel, heatramp, 0, 0);
+  }
 }
